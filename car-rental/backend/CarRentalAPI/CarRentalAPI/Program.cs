3e202830e9b06724cee2d5fa8451bbeb82370a91
kubapoke <q.paczka@gmail.com>
2024-10-28 18:27:49 +0100

    added environment variables to car rental API

--- car-rental/backend/CarRentalAPI/CarRentalAPI/Program.cs
+++ car-rental/backend/CarRentalAPI/CarRentalAPI/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Load the environment variables
+DotNetEnv.Env.Load();
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
