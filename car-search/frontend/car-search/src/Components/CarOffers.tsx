ccc7e5c7d5399de7c0e54686c312bf576b2c4580
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-11-02 00:57:47 +0100

    CarOffers
    Offers are now showing on the home page.

---
+++ car-search/frontend/car-search/src/Components/CarOffers.tsx
@@ -0,0 +1,40 @@
+import {useState, useEffect} from "react";
+import CarOffer from './CarOffer.tsx'
+
+const CarOffers = ({isHome = false}) => {
+    const [offers, setOffers] = useState([]);
+
+    useEffect(() => {
+        const fetchOffers = async () => {
+            const apiUrl = isHome ? '/api/cars?_limit=3' : '/api/cars';
+
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                setOffers(data);
+            }
+            catch (error){
+                console.log('Error fetching data', error);
+            }
+        }
+
+        fetchOffers();
+    }, []);
+    
+    return (
+        <section className="bg-blue-50 px-4 py-10">
+            <div className="container-xl lg:container m-auto">
+                <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+                    You might like:
+                </h2>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {offers.map((offer) => (
+                            <CarOffer key={offer.id} offer={offer}/>
+                        ))}
+                    </div>
+            </div>
+        </section>
+    );
+};
+
+export default CarOffers;
--- car-search/frontend/car-search/src/Pages/HomePage.tsx
+++ car-search/frontend/car-search/src/Pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import Hero from "../Components/Hero.tsx";
 import SearchBar from "../Components/SearchBar.tsx";
+import CarOffers from "../Components/CarOffers.tsx";
 
 const HomePage = () => {
     return (
         <>
             <Hero/>
             <SearchBar/>
+            <CarOffers isHome={true}/>
         </>
     );
 };
