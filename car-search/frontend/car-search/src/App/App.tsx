fed8257ea8bee158507d8e6f8003a069724734d8
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-11-02 12:26:53 +0100

    Browse Offers
    Added a BrowseOffersPage that pops us after clicking MoreOffersButton. As for now the search bar does nothing.

--- car-search/frontend/car-search/src/App/App.tsx
+++ car-search/frontend/car-search/src/App/App.tsx
@@ -8,12 +8,14 @@ import {
 import MainLayout from "../Layouts/MainLayout.tsx";
 import HomePage from "../Pages/HomePage.tsx";
 import NotFoundPage from "../Pages/NotFoundPage.tsx";
+import BrowseOffersPage from "../Pages/BrowseOffersPage.tsx";
 
 function App() {
   const router = createBrowserRouter(
       createRoutesFromElements(
           <Route path="/" element={<MainLayout/>}>
               <Route index element={<HomePage/>}/>
+              <Route path="/offers" element={<BrowseOffersPage/>}/>
               <Route path="*" element={<NotFoundPage/>}/>
           </Route>
       )
---
+++ car-search/frontend/car-search/src/Pages/BrowseOffersPage.tsx
@@ -0,0 +1,13 @@
+import SearchBar from "../Components/SearchBar.tsx";
+import CarOffers from "../Components/CarOffers.tsx";
+
+const BrowseOffersPage = () => {
+    return (
+        <>
+            <SearchBar/>
+            <CarOffers/>
+        </>
+    );
+};
+
+export default BrowseOffersPage;
