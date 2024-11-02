e7edbf5ddd11d6f07268f6e8312b137605622d31
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-11-02 13:45:51 +0100

    Offer Page + toastify
    Added an Offer Page with details of a specific offer and a "Rent" button. Also added a toastify toast indicating that the correct function was evoked.

--- car-search/frontend/car-search/src/App/App.tsx
+++ car-search/frontend/car-search/src/App/App.tsx
@@ -9,17 +9,26 @@ import MainLayout from "../Layouts/MainLayout.tsx";
 import HomePage from "../Pages/HomePage.tsx";
 import NotFoundPage from "../Pages/NotFoundPage.tsx";
 import BrowseOffersPage from "../Pages/BrowseOffersPage.tsx";
+import OfferPage, {offerLoader} from "../Pages/OfferPage.tsx";
+import {toast} from 'react-toastify';
 
 function App() {
-  const router = createBrowserRouter(
+    
+    const rentCar = (id: string) => {
+        console.log('Request for car id: ', id);
+        toast.success("Request for car rental sent successfully!");
+    }
+    
+    const router = createBrowserRouter(
       createRoutesFromElements(
           <Route path="/" element={<MainLayout/>}>
               <Route index element={<HomePage/>}/>
-              <Route path="/offers" element={<BrowseOffersPage/>}/>
+              <Route path='/offers' element={<BrowseOffersPage/>}/>
+              <Route path='/offers/:id' element={<OfferPage rentCar={rentCar}/>} loader={offerLoader}/>
               <Route path="*" element={<NotFoundPage/>}/>
           </Route>
       )
-  );
+    );
   
   return <RouterProvider router={router}/>
 }
--- car-search/frontend/car-search/src/Layouts/MainLayout.tsx
+++ car-search/frontend/car-search/src/Layouts/MainLayout.tsx
@@ -1,11 +1,14 @@
 import Navbar from "../Components/Navbar.tsx";
 import { Outlet } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from 'react-toastify';
 
 const MainLayout = () => {
     return (
         <>
             <Navbar/>
             <Outlet/>
+            <ToastContainer/>
         </>
     );
 };
