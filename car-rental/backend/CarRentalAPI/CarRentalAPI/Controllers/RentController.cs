4b20b6c69dbb78eaab7f0dc143663daee981cfc7
kubapoke <q.paczka@gmail.com>
2024-11-10 19:16:39 +0100

    updated RentController.cs to just return names

--- car-rental/backend/CarRentalAPI/CarRentalAPI/Controllers/RentController.cs
+++ car-rental/backend/CarRentalAPI/CarRentalAPI/Controllers/RentController.cs
@@ -17,18 +17,25 @@ namespace CarRentalAPI.Controller
         [HttpGet("car-list")]
         public async Task<IActionResult> CarList()
         {
+            // Get all cars from the database with related Model and Brand data
             var carList = await _context.Cars
-                .GroupBy(car => car.Model)  // group by Model
-                .Select(group => new 
-                {
-                    Model = group.Key,
-                    Brand = group.Key.Brand,
-                    IsActive = group.Any(car => car.IsActive) // is any car active from that model?
-                    // should also return location
-                })
+                .Include(car => car.Model)
+                .ThenInclude(model => model.Brand)
                 .ToListAsync();
 
-            return Ok(carList);
+            // Group by Model and Brand, check if any car is active for each model
+            var distinctCarModels = carList
+                .GroupBy(car => new { ModelName = car.Model.Name, BrandName = car.Model.Brand.Name })  // Group by Model and Brand Name
+                .Select(group => new 
+                {
+                    ModelName = group.Key.ModelName,
+                    BrandName = group.Key.BrandName,
+                    IsActive = group.Any(car => car.IsActive) // True if any car of this model is active
+                    // Should also return location
+                })
+                .ToList();
+
+            return Ok(distinctCarModels);
         }
     }
 }
