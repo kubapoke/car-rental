c50ad5038b38ae138e16e0f337b955a3139e21f0
kubapoke <q.paczka@gmail.com>
2024-11-14 22:15:44 +0100

    added a basic price generator

---
+++ car-rental/backend/CarRentalAPI/CarRentalAPI/Abstractions/IPriceGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRentalAPI.Abstractions;
+
+public interface IPriceGenerator
+{
+    public decimal GeneratePrice(decimal basePrice, TimeSpan time);
+
+    public decimal GeneratePrice(decimal basePrice, DateTime startDate, DateTime endDate)
+    {
+        return GeneratePrice(basePrice, endDate.Subtract(startDate));
+    }
+}
---
+++ car-rental/backend/CarRentalAPI/CarRentalAPI/Services/PricePerDayToHourGeneratorService.cs
@@ -0,0 +1,12 @@
+﻿using CarRentalAPI.Abstractions;
+
+namespace CarRentalAPI.Services;
+
+public class PricePerDayToHourGeneratorService : IPriceGenerator
+{
+    // implementation assumes, that price given is per day, but the result is given with accuracy to an hour
+    public decimal GeneratePrice(decimal basePrice, TimeSpan time)
+    {
+        return basePrice * (decimal)time.Hours / 24m;
+    }
+}
