137fb7a75ccc9213a8a152f812bb341f549fba6b
Yahor Kachanouski <kegor2323@gmail.com>
2024-11-15 11:59:27 +0100

    IConfirmationTokenService added, prototype of implementation of this interface for Jwt token added. JWT_CONFIRMATION_TOKEN_SECRET_KEY is now required in .env

---
+++ car-rental/backend/CarRentalAPI/CarRentalAPI/Services/JwtConfirmationTokenService.cs
@@ -0,0 +1,23 @@
+﻿using CarRentalAPI.Abstractions;
+using CarRentalAPI.DTOs.Offers;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CarRentalAPI.Services
+{
+    public class JwtConfirmationTokenService : IConfirmationTokenService
+    {
+        public string GenerateConfirmationToken(OfferInfoForNewRentDto info)
+        {
+            var tokenHandelr = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_CONFIRMATION_TOKEN_SECRET_KEY"));
+            return "";
+        }
+
+        public ClaimsPrincipal ValidateConfirmationToken(string token)
+        {
+            return new ClaimsPrincipal();
+        }
+    }
+}
