3d3d1ef686cbe65dc02cfddbb3829aa9599b36b1
Yahor Kachanouski <kegor2323@gmail.com>
2024-11-15 22:06:37 +0100

    GenerateConfirmationToken method added (first version)

--- car-search/backend/CarSearchAPI/CarSearchAPI/Services/JwtConfirmationTokenService.cs
+++ car-search/backend/CarSearchAPI/CarSearchAPI/Services/JwtConfirmationTokenService.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using CarSearchAPI.DTOs.CarRental;
 using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CarSearchAPI.Services
 {
@@ -13,7 +14,24 @@ namespace CarSearchAPI.Services
             var tokenHandelr = new JwtSecurityTokenHandler();
             var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_CONFIRMATION_TOKEN_SECRET_KEY"));
 
-            return "";
+            var claims = new[]
+            {
+                new Claim("Email", info.Email),
+                new Claim("Price", info.Price.ToString()),
+                new Claim("CompanyName", info.CompanyName),
+                new Claim("StartDate", info.StartDate.ToString()),
+                new Claim("EndDate", info.EndDate.ToString())
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims), 
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature), 
+                Expires = DateTime.UtcNow.AddMinutes(10)
+            };
+            var token = tokenHandelr.CreateToken(tokenDescriptor);
+
+            return tokenHandelr.WriteToken(token);
         }
 
         public ClaimsPrincipal ValidateConfirmationToken(string token)
