7689aed6d459987e9e9c78eb90df0ee1d56af601
Yahor Kachanouski <kegor2323@gmail.com>
2024-11-16 00:17:33 +0100

    Email content now is more formal

--- car-search/backend/CarSearchAPI/CarSearchAPI/Services/SendGridEmailService.cs
+++ car-search/backend/CarSearchAPI/CarSearchAPI/Services/SendGridEmailService.cs
@@ -28,11 +28,13 @@ namespace CarSearchAPI.Services
             string myAddress = Environment.GetEnvironmentVariable("MY_ADDRESS");
             string confirmationLink = myAddress + $"/api/Rents/new-rent-confirm?token={Uri.EscapeDataString(token)}";
 
-            var htmlContent = $"<div>Your rent:" +
-                                  $"From: {info.CompanyName}\n" +  
-                                  $"Price: {info.Price}\n" +
-                                  $"Click here to confirm your rent: {confirmationLink}" + 
-                              $"</div>";
+            var htmlContent = $"<ul>Your rent:" +
+                                  $"<li>From: {info.CompanyName}</li>" +
+                                  $"<li>Your car: this is model name, which we currently don't have</li>" +
+                                  $"<li>Price: {info.Price}</li>" +
+                                  $"<li>Rent starts: {info.StartDate}</li>" +
+                                  $"<li>Rent ends: {info.EndDate}</li>" +                                   
+                              $"</ul><br/>" + $"<div>Click here to confirm your rent: {confirmationLink}</div>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
 
