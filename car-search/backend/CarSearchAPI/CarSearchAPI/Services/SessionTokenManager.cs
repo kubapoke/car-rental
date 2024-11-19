78c3b01513fc1d845e43d50ecec37bfe0473795d
Yahor Kachanouski <kegor2323@gmail.com>
2024-11-19 23:19:24 +0100

    New claims added to session token

--- car-search/backend/CarSearchAPI/CarSearchAPI/Services/SessionTokenManager.cs
+++ car-search/backend/CarSearchAPI/CarSearchAPI/Services/SessionTokenManager.cs
@@ -14,19 +14,25 @@ namespace CarSearchAPI.Services
         {
             var tokenHandelr = new JwtSecurityTokenHandler();
             var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_TOKEN_SECRET_KEY"));
-            var claim = new Claim(JwtRegisteredClaimNames.Email, email); // claims represent information, which will be inside token
+            List<Claim> claims = new List<Claim>();
+            Claim emailClaim = new Claim(JwtRegisteredClaimNames.Email, email);
+            claims.Add(emailClaim); // claims represent information, which will be inside token
             double expirationMinutes; // that is time, in which token will be valid
             if (isTemporary)
             {
+                Claim protoUserClaim = new Claim("ProtoUser", "");
+                claims.Add(protoUserClaim);
                 expirationMinutes = 10;
             }
             else
             {
+                Claim legitUserClaim = new Claim("LegitUser", "");
+                claims.Add(legitUserClaim);
                 expirationMinutes = 60;
             }
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new[] { claim }), // add claims to the token
+                Subject = new ClaimsIdentity(claims), // add claims to the token
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature), // add key
                 Expires = DateTime.UtcNow.AddMinutes(expirationMinutes) // add time
             };
