e2cf8816f0f0b1ee8ab3a2b6f3034d1c0d0c95e7
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-11-20 18:50:06 +0100

    Added Dynamic Header

--- car-search/frontend/car-search/src/Components/CarOffers.tsx
+++ car-search/frontend/car-search/src/Components/CarOffers.tsx
@@ -47,6 +47,8 @@ const CarOffers = ({isHome} : {isHome : boolean}) => {
         fetchOffers();
     }, [filters, setOffers]);
 
+    
+    const header = isHome? (offers.length > 0 ? "Recently viewed": "Tell us what you're looking for")  : "Search results:";
     // Limit offers to 3 if isHome is true
     const displayedOffers = isHome ? offers.slice(0, 3) : offers;
     
@@ -54,7 +56,7 @@ const CarOffers = ({isHome} : {isHome : boolean}) => {
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    You might like:
+                    {header}
                 </h2>
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                         {displayedOffers.map((offer) => (
