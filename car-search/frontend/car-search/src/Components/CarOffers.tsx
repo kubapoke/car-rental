bb2669d2cc3f8dde9b9f59b6d23958a6af52d2e2
kubapoke <q.paczka@gmail.com>
2024-11-21 22:38:18 +0100

    added context-dependent offer page header

--- car-search/frontend/car-search/src/Components/CarOffers.tsx
+++ car-search/frontend/car-search/src/Components/CarOffers.tsx
@@ -49,12 +49,28 @@ const CarOffers = ({isHome} : {isHome : boolean}) => {
 
     // Limit offers to 3 if isHome is true
     const displayedOffers = isHome ? offers.slice(0, 3) : offers;
+
+    const getHeader = () =>
+    {
+        if(isHome){
+            if(offers.length > 0)
+                return "Recently viewed:";
+            else
+                return "Tell us what you are looking for";
+        }
+        else{
+            if(offers.length > 0)
+                return "Search results:";
+            else
+                return "No offers matching your requirements have been found :(";
+        }
+    }
     
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    You might like:
+                    {getHeader()}
                 </h2>
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                         {displayedOffers.map((offer) => (
