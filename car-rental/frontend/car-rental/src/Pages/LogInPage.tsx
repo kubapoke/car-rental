37aa72a0fa97531692cef1ed9047f04f5d48c63b
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-11-28 00:52:33 +0100

    Cool Log-in Page
    Designed a cool log-in Page

--- car-rental/frontend/car-rental/src/Pages/LogInPage.tsx
+++ car-rental/frontend/car-rental/src/Pages/LogInPage.tsx
@@ -1,17 +1,46 @@
 import {useAuth} from "../Context/AuthContext.tsx";
 import {useNavigate} from "react-router-dom";
+import {FaLock} from 'react-icons/fa';
+
+const LockComponent = () => {
+    return (
+        <div className="flex items-center justify-center">
+            <span className="text-3xl text-white fa-solid"> <FaLock /> </span>
+        </div>
+    )
+}
+
 
 const LogInPage = () => {
-    const { isLoggedIn, setIsLoggedIn } = useAuth();
+    const {isLoggedIn, setIsLoggedIn} = useAuth();
     const navigate = useNavigate()
-    return (
-        <div className="text-indigo-800 text-xl">
-            LogInPage
-            <button onClick={() => {setIsLoggedIn(true); console.log(isLoggedIn); navigate("/logged-in/cockpit")}}>
-                Log In
-            </button>
+    return (<div className="grid grid-cols-3 grid-rows-3 min-h-screen bg-black">
+            <LockComponent/>
+            <LockComponent/>
+            <LockComponent/>
+            <LockComponent/>
+            
+            <div className="flex items-center justify-center">
+                <button
+                    onClick={() => {
+                        setIsLoggedIn(true);
+                        console.log(isLoggedIn);
+                        navigate("/logged-in/cockpit");
+                    }}
+                    className="bg-white text-black py-2 px-4 rounded"
+                >
+                    Log In
+                </button>
+            </div>
+            
+            <LockComponent/>
+            <LockComponent/>
+            <LockComponent/>
+            <LockComponent/>
         </div>
+
     );
 };
+
 
 export default LogInPage;
