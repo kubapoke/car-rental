039a11535525ddd410e9412dcb5494713e8794d6
Yahor Kachanouski <kegor2323@gmail.com>
2024-11-28 21:56:16 +0100

    AuthContext added

---
+++ car-rental/frontend/car-rental/src/Context/AuthContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface AuthContext {
+    isLoggedIn: boolean;
+    setIsLoggedIn: (isLoggedIn: boolean) => void;
+}
+
+const AuthContext = createContext<AuthContext | undefined>(undefined);
+
+export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(!!sessionStorage.getItem("authToken"));
+
+    return (
+        <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+};
