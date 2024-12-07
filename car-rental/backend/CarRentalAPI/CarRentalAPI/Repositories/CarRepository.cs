d60aaf91fb35704a1d7dbd6cfd63ae4908f351a2
Yahor Kachanouski <kegor2323@gmail.com>
2024-12-07 20:57:04 +0100

    CarRepository and its inteface added

---
+++ car-rental/backend/CarRentalAPI/CarRentalAPI/Repositories/CarRepository.cs
@@ -0,0 +1,22 @@
+﻿using CarRentalAPI.Abstractions.Repositories;
+using CarRentalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalAPI.Repositories
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly CarRentalDbContext _context;
+
+        public CarRepository(CarRentalDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Car>> GetCarsByIdAsync(List<int> ids)
+        {
+            var cars = await _context.Cars.Where(c => ids.Contains(c.CarId)).ToListAsync();
+            return cars;
+        }
+    }
+}
