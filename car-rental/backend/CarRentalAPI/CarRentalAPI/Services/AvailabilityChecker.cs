b7e5a13059eb90ba9a77df3d57de584197c0bac6
kubapoke <q.paczka@gmail.com>
2024-12-10 02:14:49 +0100

    changed the method name appropriately

--- car-rental/backend/CarRentalAPI/CarRentalAPI/Services/AvailabilityChecker.cs
+++ car-rental/backend/CarRentalAPI/CarRentalAPI/Services/AvailabilityChecker.cs
@@ -9,12 +9,12 @@ namespace CarRentalAPI.Services
             List<int> notAvailableCarIds = new List<int>();
             foreach (var pair in pairs)
             {
-                if (IsIntervalsCollide(pair.StartDate, pair.EndDate, startDate, endDate)) notAvailableCarIds.Add(pair.CarId);
+                if (DoIntervalsCollide(pair.StartDate, pair.EndDate, startDate, endDate)) notAvailableCarIds.Add(pair.CarId);
             }
             return notAvailableCarIds;
         }
 
-        private bool IsIntervalsCollide(DateTime start1,  DateTime end1, DateTime start2, DateTime end2)
+        private bool DoIntervalsCollide(DateTime start1,  DateTime end1, DateTime start2, DateTime end2)
         {
             // Check if interval 2 collides with interval 1
             if ((end2 <= end1) && (end2 >= start1)) return true;
