5628ae388a35e4d4de8d2985e93f277b87bd98b6
kubapoke <q.paczka@gmail.com>
2024-12-13 21:45:34 +0100

    removed unnecessary methods from OfferRepository.cs

--- car-rental/backend/CarRentalAPI/CarRentalAPI/Repositories/OfferRepository.cs
+++ car-rental/backend/CarRentalAPI/CarRentalAPI/Repositories/OfferRepository.cs
@@ -19,64 +19,6 @@ namespace CarRentalAPI.Repositories
             _priceGenerator = priceGenerator;
         }
 
-        public async Task<string> CreateOfferForRedisAsync(Car car, DateTime startDate, DateTime endDate, string conditions,
-            string companyName)
-        {
-            var offerGuid = Guid.NewGuid().ToString();
-            
-            OfferForRedisDto redisOffer = new OfferForRedisDto
-            {
-                CarId = car.CarId,
-                Brand = car.Model.Brand.Name,
-                Model = car.Model.Year == null ? car.Model.Name : car.Model.Name + " " + car.Model.Year,
-                Price = await _priceGenerator.GeneratePriceAsync(car.Model.BasePrice, startDate, endDate),
-                Conditions = conditions,
-                CompanyName = companyName,
-                Location = car.Location,
-                StartDate = startDate,
-                EndDate = endDate,
-            };
-            
-            var serializedOffer = JsonConvert.SerializeObject(redisOffer);
-            
-            await _redisCacheService.SetValueAsync(offerGuid, serializedOffer, TimeSpan.FromMinutes(15));
-
-            return offerGuid;
-        }
-
-        public async Task<OfferForCarSearchDto?> CreateCarSearchOfferFromRedisOfferAsync(string offerGuid, string email)
-        {
-            var offerJson = await _redisCacheService.GetValueAndRefreshExpiryAsync(offerGuid, TimeSpan.FromMinutes(15));
-
-            if (offerJson == null)
-            {
-                throw new ArgumentNullException(nameof(offerJson));
-            }
-            
-            var redisOffer = JsonConvert.DeserializeObject<OfferForRedisDto>(offerJson);
-
-            if (redisOffer == null)
-            {
-                throw new ArgumentNullException(nameof(offerJson));
-            }
-            
-            OfferForCarSearchDto offer = new OfferForCarSearchDto
-            {
-                OfferId = offerGuid,
-                Brand = redisOffer.Brand,
-                Model = redisOffer.Model,
-                Price = redisOffer.Price,
-                Conditions = redisOffer.Conditions,
-                CompanyName = redisOffer.CompanyName,
-                Location = redisOffer.Location,
-                StartDate = redisOffer.StartDate,
-                EndDate = redisOffer.EndDate,
-                Email = email,
-            };
-            
-            return offer;
-        }
-
         public async Task<List<OfferForCarSearchDto>> CreateAndRetrieveOffersAsync(List<Car> cars, DateTime startDate, DateTime endDate, 
             string conditions, string companyName, string email)
         {
