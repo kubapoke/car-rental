2d7bdf5fa0ce5241990328c45bf4a0beef4d9993
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-12-15 10:37:12 +0100

    Return Button in RentTile

--- car-search/frontend/car-search/src/Components/UserRentTile.tsx
+++ car-search/frontend/car-search/src/Components/UserRentTile.tsx
@@ -2,6 +2,10 @@ import {FaCar} from "react-icons/fa";
 import {Rent} from "../Context/RentsContext.tsx";
 
 const UserRentTile = ({rent}: { rent: Rent }) => {
+    function handleReturn() {
+        
+    }
+
     return (
         <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
@@ -13,9 +17,22 @@ const UserRentTile = ({rent}: { rent: Rent }) => {
                 <div className="border border-gray-100 mb-5"></div>
 
                 <div className="text-xl">
-                    <h4 className="text-bold">Wypożyczono:</h4>
-                    <p>od {rent.startDate.slice(0,10)}</p>
-                    <p>do {rent.endDate.slice(0,10)}</p>
+                    <h4 className="text-bold">Rented:</h4>
+                    <div className="flex items-center justify-between">
+                        <div className="flex flex-col">
+                            <div className="flex">
+                                <span className="w-12">from</span>
+                                <p>{rent.startDate.slice(0, 10)}</p>
+                            </div>
+                            <div className="flex">
+                                <span className="w-12">to</span>
+                                <p>{rent.endDate.slice(0, 10)}</p>
+                            </div>
+                        </div>
+                        <button 
+                            onClick={handleReturn}
+                            className="bg-blue-500 text-white rounded-lg p-2 m-4 mb-8">Return</button>
+                    </div>
                 </div>
             </div>
         </div>
