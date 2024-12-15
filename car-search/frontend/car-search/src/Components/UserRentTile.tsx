311221a52bfb1222389f5d382c69016613b2f686
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-12-15 17:54:18 +0100

    Rent Tile style for Returned
    For now it's hardcoded for all rents to be active but you just have to change the definition of isReturned bool (there's a TODO comment)

--- car-search/frontend/car-search/src/Components/UserRentTile.tsx
+++ car-search/frontend/car-search/src/Components/UserRentTile.tsx
@@ -3,13 +3,16 @@ import {Rent, useRents} from "../Context/RentsContext.tsx";
 
 const UserRentTile = ({rent}: { rent: Rent }) => {
     const { returnCar} = useRents();
+    const isReturned = true; //TODO change this to check if the car is returned
     
     return (
-        <div className="bg-white rounded-xl shadow-md relative">
+        <div className={`rounded-xl shadow-md relative ${isReturned ? 'bg-gray-300' : 'bg-white'}`}>
             <div className="p-4">
                 <div className="flex items-center justify-center">
                     <FaCar className="text-indigo-500 text-xl mr-3"/>
-                    <h3 className="text-xl font-bold">{rent.brand} {rent.model}</h3>
+                    <h3 className="text-xl font-bold">
+                        {rent.brand} {rent.model} {isReturned && '(Returned)'}
+                    </h3>
                 </div>
 
                 <div className="border border-gray-100 mb-5"></div>
@@ -27,9 +30,13 @@ const UserRentTile = ({rent}: { rent: Rent }) => {
                                 <p>{rent.endDate.slice(0, 10)}</p>
                             </div>
                         </div>
-                        <button 
-                            onClick={()=>returnCar(rent.rentId)}
-                            className="bg-blue-500 text-white rounded-lg p-2 m-4 mb-8">Return</button>
+                        {!isReturned && (
+                            <button
+                                onClick={() => returnCar(rent.rentId)}
+                                className="bg-blue-500 text-white rounded-lg p-2 m-4 mb-8">
+                                Return
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
