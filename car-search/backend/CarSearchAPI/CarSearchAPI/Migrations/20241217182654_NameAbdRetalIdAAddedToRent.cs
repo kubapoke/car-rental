f68e7fafa389b6999cd2bccead84f1036e6c300b
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-12-17 19:27:36 +0100

    Junk Commit

---
+++ car-search/backend/CarSearchAPI/CarSearchAPI/Migrations/20241217182654_NameAbdRetalIdAAddedToRent.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarSearchAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameAbdRetalIdAAddedToRent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RentalCompanyName",
+                table: "rents",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RentalCompanyRentId",
+                table: "rents",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "rents",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RentalCompanyName",
+                table: "rents");
+
+            migrationBuilder.DropColumn(
+                name: "RentalCompanyRentId",
+                table: "rents");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "rents");
+        }
+    }
+}
