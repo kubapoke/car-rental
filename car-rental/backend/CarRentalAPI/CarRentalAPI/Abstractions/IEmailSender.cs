fb3bbb4a7c71ae6c47a6ce38dc37002aecc42cd9
Yahor Kachanouski <kegor2323@gmail.com>
2024-12-17 15:05:38 +0100

    IEmailSender interface added

---
+++ car-rental/backend/CarRentalAPI/CarRentalAPI/Abstractions/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using CarRentalAPI.Models;
+
+namespace CarRentalAPI.Abstractions
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendReturnConfirmedEmailAsync(Rent rent);
+    }
+}
